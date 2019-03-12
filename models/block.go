968d04cde02c44da2cab68adcaceaa324b648212
Daniil Dulin <daniildulin@gmail.com>
2019-03-12 16:39:52 +0300

    update models

--- models/block.go
+++ models/block.go
@@ -6,14 +6,14 @@ import (
 
 type Block struct {
 	ID                  uint64                `json:"id"`
-	TotalTxs            uint64                `json:"txTotal" sql:"default:0"`
+	TotalTxs            uint64                `json:"total_txs" sql:"default:0"`
 	Size                uint64                `json:"size"`
 	ProposerValidatorID uint64                `json:"proposer_validator_id"`
-	NumTxs              uint32                `json:"txCount" sql:"default:0"`
-	BlockTime           uint64                `json:"blockTime"`
-	CreatedAt           time.Time             `json:"createdAt"`
-	UpdatedAt           time.Time             `json:"updatedAt"`
-	BlockReward         string                `json:"blockReward" sql:"type:numeric(70)"`
+	NumTxs              uint32                `json:"num_txs" sql:"default:0"`
+	BlockTime           uint64                `json:"block_time"`
+	CreatedAt           time.Time             `json:"created_at"`
+	UpdatedAt           time.Time             `json:"updated_at"`
+	BlockReward         string                `json:"block_reward" sql:"type:numeric(70)"`
 	Hash                string                `json:"hash"`
 	Proposer            *Validator            `json:"proposer" pg:"fk:proposer_validator_id"`    //relation has one to Validators
 	Validators          []*Validator          `json:"validators" pg:"many2many:block_validator"` //relation has many to Validators
@@ -21,6 +21,7 @@ type Block struct {
 	InvalidTransactions []*InvalidTransaction `json:"invalid_transactions"`                      //relation has many to InvalidTransactions
 	Rewards             []*Reward             `json:"rewards"`                                   //relation has many to Rewards
 	Slashes             []*Slash              `json:"slashes"`                                   //relation has many to Slashes
+	BlockValidators     []BlockValidator      `json:"block_validators"`
 }
 
 //Return block hash with prefix
