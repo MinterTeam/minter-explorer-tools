a8fdec4479bdbee5974232c0a7e6131a4d0ddbd6
Daniil Dulin <daniildulin@gmail.com>
2019-03-13 11:29:15 +0300

    update model ExtenderEnvironment

--- models/extender_environment.go
+++ models/extender_environment.go
@@ -1,14 +1,23 @@
 package models
 
 type ExtenderEnvironment struct {
-	Debug       bool
-	DbName      string
-	DbUser      string
-	DbPassword  string
-	NodeApi     string
-	WsLink      string
-	WsKey       string
-	ApiHost     string
-	ApiPort     int
-	TxChunkSize int
+	AppName                  string
+	Debug                    bool
+	DbName                   string
+	DbUser                   string
+	DbPassword               string
+	DbMinIdleConns           int
+	DbPoolSize               int
+	NodeApi                  string
+	ApiHost                  string
+	ApiPort                  int
+	TxChunkSize              int
+	EventsChunkSize          int
+	WrkSaveRewardsCount      int
+	WrkSaveSlashesCount      int
+	WrkSaveTxsCount          int
+	WrkSaveTxsOutputCount    int
+	WrkSaveInvTxsCount       int
+	WrkSaveAddressesCount    int
+	WrkSaveValidatorTxsCount int
 }
