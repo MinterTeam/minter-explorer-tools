5ee8669c967457addc1a8ec5b9af60a2bec99474
Daniil Dulin <daniildulin@gmail.com>
2019-03-18 13:15:54 +0300

    update model BlockValidator

--- models/block_validator.go
+++ models/block_validator.go
@@ -4,6 +4,6 @@ type BlockValidator struct {
 	tableName   struct{}  `sql:"block_validator"`
 	BlockID     uint64    `json:"block_id"`
 	ValidatorID uint64    `json:"validator_id"`
-	Signed      *bool     `json:"signed"`
+	Signed      bool      `json:"signed"`
 	Validator   Validator `json:"validator"`
 }
