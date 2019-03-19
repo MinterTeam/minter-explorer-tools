533091d045877e86ab0b3a923ecdbaa6369f2936
Daniil Dulin <daniildulin@gmail.com>
2019-03-19 12:38:15 +0300

    update Coin model

--- models/coin.go
+++ models/coin.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Coin struct {
-	ID                    uint64    `json:"id"`
-	CreationAddressID     uint64    `json:"creation_address_id"`
-	CreationTransactionID uint64    `json:"creation_transaction_id"`
-	DeletedAtBlockID      *uint64   `json:"deleted_at_block_id"`
-	Crr                   uint64    `json:"crr"`
-	UpdatedAt             time.Time `json:"updated_at"`
-	Volume                string    `json:"volume"          sql:"type:numeric(70)"`
-	ReserveBalance        string    `json:"reserve_balance" sql:"type:numeric(70)"`
-	Name                  string    `json:"name"            sql:"type:varchar(255)"`
-	Symbol                string    `json:"symbol"          sql:"type:varchar(20)"`
+	ID                    uint64     `json:"id"`
+	CreationAddressID     uint64     `json:"creation_address_id"`
+	CreationTransactionID uint64     `json:"creation_transaction_id"`
+	Crr                   uint64     `json:"crr"`
+	Volume                string     `json:"volume"          sql:"type:numeric(70)"`
+	ReserveBalance        string     `json:"reserve_balance" sql:"type:numeric(70)"`
+	Name                  string     `json:"name"            sql:"type:varchar(255)"`
+	Symbol                string     `json:"symbol"          sql:"type:varchar(20)"`
+	UpdatedAt             time.Time  `json:"updated_at"`
+	DeletedAt             *time.Time `json:"deleted_at"`
 }
