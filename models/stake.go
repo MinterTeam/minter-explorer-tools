7452d4ae3866c6344637e9d46af491dd9fc8a97a
Daniil Dulin <daniildulin@gmail.com>
2019-03-26 14:26:06 +0300

    change Stake PK

--- models/stake.go
+++ models/stake.go
@@ -1,10 +1,9 @@
 package models
 
 type Stake struct {
-	ID             uint64     `json:"id" sql:",pk"`
-	OwnerAddressID uint64     `json:"owner_address_id"`
-	ValidatorID    uint64     `json:"validator_id"`
-	CoinID         uint64     `json:"coin_id"`
+	OwnerAddressID uint64     `json:"owner_address_id" sql:",pk"`
+	ValidatorID    uint64     `json:"validator_id" sql:",pk"`
+	CoinID         uint64     `json:"coin_id" sql:",pk"`
 	Value          string     `json:"value"     sql:"type:numeric(70)"`
 	BipValue       string     `json:"bip_value" sql:"type:numeric(70)"`
 	Coin           *Coin      `json:"coins"`                                  //Relation has one to Coins
