47795858ab5b390feb522b7c7ffe153f2e197013
Daniil Dulin <daniildulin@gmail.com>
2019-04-15 13:28:34 +0300

    exclude rawdata from sql

--- models/transaction.go
+++ models/transaction.go
@@ -33,7 +33,7 @@ type Transaction struct {
 	Hash          string               `json:"hash"`
 	ServiceData   string               `json:"service_data"`
 	Data          interface{}          `json:"data"`
-	RawData       json.RawMessage      `json:"-"`
+	RawData       json.RawMessage      `json:"-" sql:"-"`
 	Tags          map[string]string    `json:"tags"`
 	Payload       []byte               `json:"payload"`
 	RawTx         []byte               `json:"raw_tx"`
