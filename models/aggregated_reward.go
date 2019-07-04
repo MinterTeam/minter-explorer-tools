425c106e170cbc98a26f646657ff8ebe456f9a17
Daniil Dulin <daniildulin@gmail.com>
2019-07-04 13:05:23 +0300

    fix AggregatedReward

--- models/aggregated_reward.go
+++ models/aggregated_reward.go
@@ -1,13 +1,15 @@
 package models
 
+import "time"
+
 type AggregatedReward struct {
-	FromBlockID  uint64    `json:"from_block_id" sql:",pk"`
-	ToBlockID    uint64    `json:"to_block_id"`
-	AddressID    uint64    `json:"address_id"    sql:",pk"`
-	ValidatorID  uint64    `json:"validator_id"  sql:",pk"`
-	Role         string    `json:"role"          sql:",pk"`
-	Amount       string    `json:"amount"        sql:"type:numeric(70)"`
-	CreatedAt    time.Time `json:"created_at"`
+	FromBlockID  uint64     `json:"from_block_id" sql:",pk"`
+	ToBlockID    uint64     `json:"to_block_id"`
+	AddressID    uint64     `json:"address_id"    sql:",pk"`
+	ValidatorID  uint64     `json:"validator_id"  sql:",pk"`
+	Role         string     `json:"role"          sql:",pk"`
+	Amount       string     `json:"amount"        sql:"type:numeric(70)"`
+	CreatedAt    time.Time  `json:"created_at"`
 	FromBlock    *Block     //Relation has one to Blocks
 	ToBlockBlock *Block     //Relation has one to Blocks
 	Address      *Address   //Relation has one to Addresses
