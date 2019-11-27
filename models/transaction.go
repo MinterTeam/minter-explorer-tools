3a571bb9d10df3d3f24ba16f9f512d18a0236368
Daniil Dulin <daniildulin@gmail.com>
2019-11-27 17:05:47 +0300

    update CreateMultisigTxData model

--- models/transaction.go
+++ models/transaction.go
@@ -122,8 +122,8 @@ type EditCandidateTxData struct {
 }
 
 type CreateMultisigTxData struct {
-	Threshold uint64   `json:"threshold"`
-	Weights   []uint64 `json:"weights"`
+	Threshold uint32   `json:"threshold"`
+	Weights   []uint32 `json:"weights"`
 	Addresses []string `json:"addresses"`
 }
 
